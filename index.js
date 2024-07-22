edebe1ef7d28eb7de9b12a23579c6fd07a5bcece
MalenaFG <malena.f.giusti@gmail.com>
2024-07-22 14:59:46 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    let getValue = this.items[pos]
 
-  max() {}
+    if (!getValue) {
+      throw new Error("OutOfBounds")
+    }
 
-  min() {}
+    return getValue
+  }
 
-  sum() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList")
+    }
 
-  avg() {}
+    return this.items.slice(-1)
+  }
+
+  min() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList")
+    }
+
+    return this.items[0]
+  }
+
+  sum() {
+
+    if (!this.items.length) {
+      return 0
+    }
+
+    let sumValue = this.items.reduce((acc, elm) => acc + elm)
+
+    return sumValue
+  }
+
+  avg() {
+
+    if (!this.items.length) {
+      throw new Error("EmptySortedList")
+    }
+
+    let average = this.items.reduce((acc, elm) => acc + elm) / this.items.length
+
+    return average
+  }
 }
 
 module.exports = SortedList;
